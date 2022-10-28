64d8f7a584c8cd354b946cd8afb31da33d351c04
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-10-27 22:25:41 -0400

    added base level app and index js code to get it up and running/functional

---
+++ client/src/app.js
@@ -0,0 +1,12 @@
+import "./app.css";
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h2 className="title-header"> Noteify </h2>
+      </header>
+    </div>
+  );
+};
+
+export default App;

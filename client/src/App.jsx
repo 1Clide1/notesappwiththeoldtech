3877d68d8a22c7939168c6d8e249829244711b38
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-11-06 23:04:11 -0500

    renamed app.js to .jsx to help readiblity to understand which files are more html/javascript based

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles/app.scss";
+const App = () => {
+  return (
+    <header className="app-header">
+      <h2 className="title-header"> Noteify </h2>
+    </header>
+  );
+};
+
+export default App;

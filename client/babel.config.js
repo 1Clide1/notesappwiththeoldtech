0e91ffd56f010922080d5f7f772b82d7e297d70e
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-11-06 20:07:21 -0500

    added the babel config to the project, have it set up for the code needed

---
+++ client/babel.config.js
@@ -0,0 +1,20 @@
+// config for babel
+
+const plugins = [];
+
+// if it is not production add react refresh because it cannot go in production
+if (process.env.NODE_ENV !== "production") {
+  plugins.push("react-refresh/babel");
+}
+
+// like webpack contents get exported out
+module.exports = {
+  // adding the babel preset
+  presets: [
+    "@babel/preset-env",
+    // Runtime automatic with React 17+ allows not importing React
+    // in files only using JSX (no state or React methods)
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+  plugins: plugins,
+};

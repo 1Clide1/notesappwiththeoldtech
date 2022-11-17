2f3ee72ce74c481f2f84aa7ab02f90ba092419fa
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-11-16 19:34:50 -0500

    properly configured eslint and made it work with the current project, also refactored the babel config/refactored index.js to eslint standards

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,11 @@
 // imports
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./styles/index.scss";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './styles/index.scss';
 
 // creating the root
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 // rendering out the root
